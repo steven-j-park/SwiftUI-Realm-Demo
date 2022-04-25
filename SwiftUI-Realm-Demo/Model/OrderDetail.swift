25da468e19831e2539c5663570b6004c629771e0
Steven Park <steven.j.park@outlook.com>
2022-04-25 12:40:16 -0400

    Added primary key to OrderDetails

--- SwiftUI-Realm-Demo/Model/OrderDetail.swift
+++ SwiftUI-Realm-Demo/Model/OrderDetail.swift
@@ -8,6 +8,7 @@
 import RealmSwift
 
 class OrderDetail: Object, ObjectKeyIdentifiable {
+    @Persisted(primaryKey: true) var _id: ObjectId
     @Persisted(originProperty: "orderDetails") var order: LinkingObjects<Order>
     @Persisted(originProperty: "orderDetails") var item: LinkingObjects<Item>
 }
