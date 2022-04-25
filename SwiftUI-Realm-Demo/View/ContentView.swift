b6e638090332f117e3ef0b2f27d6361dfd133614
Steven Park <steven.j.park@outlook.com>
2022-04-25 14:52:51 -0400

    Move sample data loading to ContentView

--- SwiftUI-Realm-Demo/View/ContentView.swift
+++ SwiftUI-Realm-Demo/View/ContentView.swift
@@ -6,8 +6,11 @@
 //
 
 import SwiftUI
+import RealmSwift
 
 struct ContentView: View {
+    
+    
     var body: some View {
         TabView {
             CustomerList()
@@ -26,6 +29,17 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        sampleData()
+        return ContentView()
+    }
+    
+    static func sampleData() {
+        let realm = try! Realm()
+        try! realm.write {
+            realm.deleteAll()
+            realm.add(Customer(value: ["name": "Tester McTesterson"]))
+            realm.add(Customer(value: ["name": "John Doe"]))
+            realm.add(Customer(value: ["name": "Steven Park"]))
+        }
     }
 }
