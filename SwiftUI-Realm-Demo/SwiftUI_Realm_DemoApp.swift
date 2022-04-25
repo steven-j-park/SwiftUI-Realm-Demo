bc9931d3df68bd88bb45ab5ef8e776a7e4aad647
Steven Park <steven.j.park@outlook.com>
2022-04-25 11:12:32 -0400

    Initial Commit

---
+++ SwiftUI-Realm-Demo/SwiftUI_Realm_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Realm_DemoApp.swift
+//  SwiftUI-Realm-Demo
+//
+//  Created by Steven Park on 4/25/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Realm_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
