a6267b602d8d8a9094fd29c5aec5390f67b36dd1
Steven Park <steven.j.park@outlook.com>
2022-05-10 19:15:47 -0400

    Revert to default realm configuration

--- SwiftUI-Realm-Demo/SwiftUI_Realm_DemoApp.swift
+++ SwiftUI-Realm-Demo/SwiftUI_Realm_DemoApp.swift
@@ -10,16 +10,19 @@ import RealmSwift
 
 @main
 struct SwiftUI_Realm_DemoApp: SwiftUI.App {
-    var realmURL: URL
+    //var realmURL: URL
     
     init() {
-        realmURL = Bundle.main.url(forResource: "bundled", withExtension: "realm")!
+        let realm = try! Realm()
+        print("Realm is located at:", realm.configuration.fileURL!)
+        
+        //realmURL = Bundle.main.url(forResource: "bundled", withExtension: "realm")!
     }
     
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .environment(\.realmConfiguration, Realm.Configuration(fileURL: realmURL))
+                //.environment(\.realmConfiguration, Realm.Configuration(fileURL: realmURL))
         }
     }
 }
