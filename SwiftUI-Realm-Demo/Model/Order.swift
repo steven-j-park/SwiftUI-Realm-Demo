09737938c6f7e945d8ae32791a5e60fca5f6cc02
Steven Park <steven.j.park@outlook.com>
2022-05-12 20:16:12 -0400

    cleanup

--- SwiftUI-Realm-Demo/Model/Order.swift
+++ SwiftUI-Realm-Demo/Model/Order.swift
@@ -11,7 +11,6 @@ import RealmSwift
 class Order: Object, ObjectKeyIdentifiable {
     @Persisted(primaryKey: true) var _id: ObjectId
     @Persisted var orderDate = Date()
-    
     @Persisted var orderDetails: List<Item>
     
     @Persisted(originProperty: "orders") var orderedBy: LinkingObjects<Customer>
