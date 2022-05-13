3b85f4ebee7f60c100b47faef6dab1a9e7ab0aa4
Steven Park <steven.j.park@outlook.com>
2022-05-13 11:58:49 -0400

    OrderList view

---
+++ SwiftUI-Realm-Demo/View/OrderList.swift
@@ -0,0 +1,20 @@
+//
+//  OrderList.swift
+//  SwiftUI-Realm-Demo
+//
+//  Created by Steven Park on 5/13/22.
+//
+
+import SwiftUI
+
+struct OrderList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OrderList_Previews: PreviewProvider {
+    static var previews: some View {
+        OrderList()
+    }
+}
