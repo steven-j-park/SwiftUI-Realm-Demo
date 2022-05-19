5a3f7142854c78797dcc1240b20b7a4ffbecf6e2
Steven Park <steven.j.park@outlook.com>
2022-05-18 20:16:19 -0400

    new OrderDetailView

---
+++ SwiftUI-Realm-Demo/View/OrderDetail.swift
@@ -0,0 +1,20 @@
+//
+//  OrderDetail.swift
+//  SwiftUI-Realm-Demo
+//
+//  Created by Steven Park on 5/18/22.
+//
+
+import SwiftUI
+
+struct OrderDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OrderDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        OrderDetail()
+    }
+}
