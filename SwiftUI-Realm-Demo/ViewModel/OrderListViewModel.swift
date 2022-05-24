19e09828d7529adac854a085903cf32097665a1d
Steven Park <steven.j.park@outlook.com>
2022-05-23 22:42:02 -0400

    Added empty lines to maintain consistency with other viewmodel classes

--- SwiftUI-Realm-Demo/ViewModel/OrderListViewModel.swift
+++ SwiftUI-Realm-Demo/ViewModel/OrderListViewModel.swift
@@ -9,9 +9,11 @@ import Foundation
 
 extension OrderList {
     class OrderListViewModel {
+        
         func getFormattedDateTime(date: Date) -> String {
             let dateFormatter = DateFormatter()
             dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+            
             return dateFormatter.string(from: date)
         }
     }
